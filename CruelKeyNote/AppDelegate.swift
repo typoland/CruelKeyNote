cd5bc1fcf1c3a77b76fc6cfda5f94d7f5345cf5f
Łukasz Dziedzic <lukasz@LukaPro.local>
2015-03-21 17:46:06 +0100

    Initial Commit

---
+++ CruelKeyNote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CruelKeyNote
+//
+//  Created by Łukasz Dziedzic on 21/03/15.
+//  Copyright (c) 2015 Łukasz Dziedzic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
